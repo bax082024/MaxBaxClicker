a5e8765d37abecb7052eecec3afb0cf9e2beb030
bax082024 <bax082024@gmail.com>
2024-10-13 23:13:56 +0200

    added background

--- obj/Debug/net8.0/MaxBaxClicker.AssemblyInfo.cs
+++ obj/Debug/net8.0/MaxBaxClicker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaxBaxClicker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b15d47fddc974d603f99342e6f54dc23645e6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddb51eb7eb3409a4ddff4ea90765edb56ee03f2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaxBaxClicker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaxBaxClicker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
